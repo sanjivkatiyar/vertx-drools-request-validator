8ddc62c632c2ed70c5dc24384b0cfcd7e1650963
Sanjiv Katiyar <sanjiv.katiyar@gmail.com>
2024-07-21 03:50:27 -0700

    ok

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
-FROM openjdk:8-jre-alpine
+FROM openjdk:17-jdk-alpine
+MAINTAINER Sanjiv Katiyar
 
 EXPOSE 8888
 
-COPY start.sh $HOME/bin/start.sh
+COPY ./build/libs/vertx-drools-request-validator-1.0-SNAPSHOT.jar /usr/app/
+WORKDIR /usr/app
+
+ENTRYPOINT ["java", "-jar", "vertx-drools-request-validator-1.0-SNAPSHOT.jar"]
--- start.sh
+++
@@ -1,9 +0,0 @@
-echo "Printing current home: ${HOME}"
-APP_PATH = "${HOME}/bin/vertx-drools-request-validator"
-
-cd $APP_PATH || fails "Unable to change directory to $APP_PATH"
-APP_JAR=$(ls vertx-drools-request-validator-1.0-SNAPSHOT.jar)
-
-JAVA_CMD="java -cp $APP_JAR:lib/* org.sanjiv.requestvalidator.StartUp"
-echo "FINAL JAVA CMD: $JAVA_CMD"
-$JAVA_CMD
